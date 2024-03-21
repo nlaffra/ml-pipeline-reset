00b3f429221f72c4388f8070e49b6386057fd254
nlaffra <163353309+nlaffra@users.noreply.github.com>
2024-03-20 22:23:55 -0600

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -130,9 +130,7 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    X_slice, y_slice, _, _ = process_data(data, column_name, slice_value,
-                                          categorical_features, label, encoder, lb,
-                                          model)
+    X_slice, y_slice, _, _ = process_data(data, categorical_features,label,training, encoder, lb)
     preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
