b805abab60283dfce2ec91e4ea603508a963d70e
nlaffra <163353309+nlaffra@users.noreply.github.com>
2024-03-20 21:55:12 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -92,7 +92,6 @@ print(model_path)
 print("|")
 
 save_model(model, model_path)
-modelobj = open(model_path,"rb")
 
 print("model (updated): ", end="|")
 print(model)
@@ -110,10 +109,7 @@ print(encoder)
 print("|")
 
 # load the model
-model = load_model(
-    #model_path
-    modelobj
-)
+model = load_model(model_path)
 
 print("model (loaded): ", end="|")
 print(model)
