4d7b7d61f5f04c589e6b7217661c24fb5fa63543
nlaffra <163353309+nlaffra@users.noreply.github.com>
2024-03-21 18:14:00 -0600

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -3,12 +3,12 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = # Your code here
+r = requests.get("http://127.0.0.1:8000")
 
 # TODO: print the status code
-# print()
+print(r.status_code)
 # TODO: print the welcome message
-# print()
+print()
 
 
 
